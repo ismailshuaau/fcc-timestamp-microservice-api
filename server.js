6d6db343f13da0f45f1f0a39218a97e9c9d988a5
Glitch (general-collisionppp) <none>
2018-03-18 08:46:43 +0000

    🍀⛲️ Checkpoint ./server.js:752033/1431 ./package.json:752033/98

--- server.js
+++ server.js
@@ -3,7 +3,12 @@
 
 // init project
 const express = require('express')
-const app = express()
+var bodyParser = require('body-parser');
+var cors = require('cors');
+
+// Create an instance of express for our app and instantiate bodyParser and cors
+app.use(bodyParser.json());
+app.use(cors());
 
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
@@ -26,6 +31,31 @@ const dreams = [
 app.get("/dreams", (request, response) => {
   response.send(dreams)
 })
+
+
+
+// GET Call to return JSON that formats natural and unix date
+app.get('/dateValues/:dateVal', function(req,res,next) {
+  var dateVal = req.params.dateVal;
+  // Options for formatting date in natural date time
+  var dateFomattingOptions = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+  };
+
+  if(isNaN(dateVal)) {
+    var naturalDate = new Date(dateVal);
+    naturalDate = naturalDate.toLocaleDateString('en-us', dateFomattingOptions);
+    var unixDate = new Date(dateVal).getTime()/1000;
+  } else {
+    var unixDate = dateVal;
+    var naturalDate = new Date(dateVal * 1000);
+    naturalDate = naturalDate.toLocaleDateString('en-us', dateFomattingOptions);
+  }
+  res.json({unix: unixDate, natural: naturalDate});
+});
+
 
 // could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
 app.post("/dreams", (request, response) => {
